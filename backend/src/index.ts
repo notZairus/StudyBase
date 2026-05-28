f2277e5875dd9a7709d4a0b0d8bb0ab1cc9267e6
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2026-05-29 07:22:51 +0800

    Update CI pipeline name for clarity; add API endpoint to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("hello from express");
 });
 
+app.get("/api", (req, res) => {
+  res.send("hello from api!!");
+});
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
